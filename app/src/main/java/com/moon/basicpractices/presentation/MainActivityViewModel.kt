aca0db7f8e8a2a1e67eb3d12cf2a0a82ef19e25e
moontasirul <moontasirul01@gmail.com>
2024-09-07 19:20:59 +0700

    Initial commit - add commenting in viewmodel class

--- app/src/main/java/com/moon/basicpractices/presentation/MainActivityViewModel.kt
+++ app/src/main/java/com/moon/basicpractices/presentation/MainActivityViewModel.kt
@@ -18,16 +18,42 @@ class MainActivityViewModel(
 ) : ViewModel() {
 
 
+    /**
+     * It's designed to hold a list of Product objects and is initialized with an empty list.
+     * State flows are observable data holders that emit updates
+     * to their subscribers whenever the value changes.
+     */
     private val _products = MutableStateFlow<List<Product>>(emptyList())
+
+    /**
+     * Creates a public, read-only state flow
+     * named products by transforming the mutable _products using asStateFlow().
+     * This exposes the product list to observers
+     */
     val products = _products.asStateFlow()
 
 
+    /**
+     * Channels are used for communication between coroutines.
+     * This channel is designed to send a boolean value,
+     * likely indicating whether or not to display an error toast message.
+     */
     private val _showErrorToastChannel = Channel<Boolean>()
+
+    /**
+     * The receiveAsFlow() function converts the channel into a flow,
+     * allowing observers to collect values emitted by the channel.
+     */
     val showErrorToastChannel = _showErrorToastChannel.receiveAsFlow()
 
 
+    /**
+     * Initialization block of the ViewModel.
+     * Code within this block is executed when the ViewModel is created.
+     */
     init {
         viewModelScope.launch {
+            // likely returns a flow that emits the result of fetching a list of products.
             productRepository.getProductList().collectLatest { result ->
                 when (result) {
                     is Result.Error -> {
@@ -36,6 +62,8 @@ class MainActivityViewModel(
 
                     is Result.Success -> {
                         result.data?.let { product ->
+                            // _products state flow is updated with the new product list using the update function.
+                            // This will notify any observers of the state flow that the product list has changed.
                             _products.update {
                                 product
                             }
